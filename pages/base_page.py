21f4c55a4ac116d6be4dabe4b71651d430513776
kkuzm <kkuzm2000@gmail.com>
2023-08-31 21:12:37 +0300

    Checking an element on a page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
     def __init__(self, browser, url):
@@ -9,3 +10,15 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+    
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
