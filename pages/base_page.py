d076d0f7f9cd525e16578d1a632581f925363f53
kkuzm <kkuzm2000@gmail.com>
2023-08-31 20:42:23 +0300

    first test with PageObject

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,9 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 class BasePage():
-def __init__(self, browser, url):
-    self.browser = browser
-    self.url = url
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
 
-def open(self):
-    self.browser.get(self.url)
+    def open(self):
+        self.browser.get(self.url)
